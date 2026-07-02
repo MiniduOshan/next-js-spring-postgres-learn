9c293a770fc286ba7b1e7267097f1ab0796e7fa8
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-07-02 22:32:00 +0530

    init

---
+++ backend/src/main/java/com/example/taskapp/TaskController.java
@@ -0,0 +1,30 @@
+package com.example.taskapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
